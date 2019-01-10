fada074c223cbf2f81cb6abd23dffd2b9a7f5037
Bartek Wolnik <izekbw@gmail.com>
2019-01-10 03:03:30 +0100

    Homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {UtilService} from '../util.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, public utilService: UtilService) { }
+
+  games: any;
+  popupType: string;
+  popupMessage: string;
+  ajaxCall = false;
+  apiImgUrl = environment.apiImgUrl;
 
   ngOnInit() {
+    this.getAllGames();
+  }
+
+  getAllGames() {
+    this.ajaxCall = true;
+    this.http.get(environment.apiUrl+'game/get')
+      .subscribe(
+        response => {
+          this.ajaxCall = false;
+          this.games = response['games'];
+        },
+
+        error => {
+          this.ajaxCall = false;
+          this.popupType = 'error-popup';
+          this.popupMessage = 'Could not retrieve data.';
+        })
+  }
+
+  upgradeDom() {
+    // @ts-ignore
+    componentHandler.upgradeDom();
+  }
+
+  closePopup() {
+    this.popupType = '';
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api/'
+  apiUrl: 'http://127.0.0.1:8000/api/',
+  apiImgUrl: 'http://127.0.0.1:8000/img/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
